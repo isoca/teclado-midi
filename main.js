cbfdba2175bbe1a0a414af7e58f7574b9ee57d1f
isoca <baldoisabelly@gmail.com>
2022-10-12 20:51:50 -0300

    teclas dinâmicas

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}` // template string
+
+    tecla.onclick = function () {  
+        //criando uma função anônima, aqui como tá criando uma função
+        // não executa de imediato, sem o function, ele tenta executar direto 
+        // e o browser não deixa!
+        tocaSom(idAudio);
+    };
+    contador = contador + 1;
+}
+ 
